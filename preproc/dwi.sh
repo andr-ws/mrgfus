46f766d1f2b9b4bf3e0f1b7bc702262420e62914
andr-ws <108693884+andr-ws@users.noreply.github.com>
2024-11-28 16:32:55 +0000

    Update dwi.sh

--- preproc/dwi.sh
+++ preproc/dwi.sh
@@ -14,20 +14,21 @@ dwi="${der}/data/dwi"
 find "${raw}" -type d -name 'sub-*' | sort -V | while read -r dir; do
   # extract subject-id and create directory
 	sub=$(basename "${dir}")
-  mkdir -p "${dwi}/${sub}"
+  	
 
   for ses in ses-preop ses1-postop ses2-postop; do
-	  # Copy bval and bvec files	
-	  cp "${raw}/${sub}/${ses}/dwi/${sub}_dwi.bval" "${dwi}/${sub}/${sub}_${ses}_dwi.bval"
-	  cp "${raw}/${sub}/${ses}/dwi/${sub}_dwi.bvec" "${dwi}/${sub}/${sub}_${ses}_dwi.bvec"
+  	mkdir -p "${dwi}/${sub}/${ses}"
+   	# Copy bval and bvec files	
+    	cp "${raw}/${sub}/${ses}/dwi/${sub}_dwi.bval" "${dwi}/${sub}/${ses}/${sub}_dwi.bval"
+	cp "${raw}/${sub}/${ses}/dwi/${sub}_dwi.bvec" "${dwi}/${sub}/${ses}/${sub}_dwi.bvec"
 
 	# Create index file (no. of vols in the dwi set)
-	vol_count=$(wc -w < "${dwi}/${sub}/${sub}_dwi.bval")
+	vol_count=$(wc -w < "${dwi}/${sub}/${ses}/${sub}_dwi.bval")
 	index=$(yes 1 | head -n "${vol_count}" | tr '\n' ' ')
-	echo "${index}" > "${dwi}/${sub}/index.txt"
+	echo "${index}" > "${dwi}/${sub}/${ses}/index.txt"
 
 	# Create acquisition paramaters file for edc
-	dwi_json="${raw}/${sub}/dwi/${sub}_dwi.json"
+	dwi_json="${raw}/${sub}/${ses}/dwi/${sub}_dwi.json"
 	if grep -q "j-" "${dwijson}"; then 
 		ped=-1
 	else 
@@ -36,74 +37,74 @@ find "${raw}" -type d -name 'sub-*' | sort -V | while read -r dir; do
 
 	trt=$(grep -n "TotalReadoutTime" $dwi_json | sed 's/^.\{24\}//' | sed 's/.$//')
 	arr="0 $ped 0 $trt"
-    echo $arr > "${dwi}/${sub}/acqparams.txt"
-    echo 0 1 0 0 >> "${dwi}/${sub}/acqparams.txt"
+    	echo $arr > "${dwi}/${sub}/${ses}/acqparams.txt"
+    	echo 0 1 0 0 >> "${dwi}/${sub}/${ses}/acqparams.txt"
 
-    mrconvert "${raw}/${sub}/dwi/${sub}_dwi.nii.gz" \
-    -fslgrad "${dwi}/${sub}/${sub}_dwi.bvec" \
-    "${dwi}/${sub}/${sub}_dwi.bval" \
-    "${dwi}/${sub}/${sub}_dwi_tmp.mif" \
-    -force
+   	mrconvert "${raw}/${sub}/${ses}/dwi/${sub}_dwi.nii.gz" \
+    		-fslgrad "${dwi}/${sub}/${ses}/${sub}_dwi.bvec" \
+    		"${dwi}/${sub}/${ses}/${sub}_dwi.bval" \
+    		"${dwi}/${sub}/${ses}/${sub}_dwi_tmp.mif" \
+    		-force
 
-    # Pad (rather than crop) a slice if the data is odd
-    dim=$(fslinfo "${raw}/${sub}/dwi/${sub}_dwi.nii.gz" | awk '/^dim3/ {print $2}')
+    	# Pad (rather than crop) a slice if the data is odd
+    	dim=$(fslinfo "${raw}/${sub}/dwi/${ses}/${sub}_dwi.nii.gz" | awk '/^dim3/ {print $2}')
 
-    # Test if dimension is even
+    	# Test if dimension is even
 	if [ $((dim%2)) -eq 0 ]; then
 		echo "3rd dimension even, no need for padding"
 	else
 		echo "3rd dimension odd, padding..."
-		mrgrid "${dwi}/${sub}/${sub}_dwi_tmp.mif" pad -axis 2 0,1 \
-		"${dwi}/${sub}/${sub}_dwi_tmp.mif" \
+		mrgrid "${dwi}/${sub}/${ses}/${sub}_dwi_tmp.mif" pad -axis 2 0,1 \
+		"${dwi}/${sub}/${ses}/${sub}_dwi_tmp.mif" \
 		-force
 	fi
 
 	# MP-PCA denoising
 	dwidenoise \
-	"${dwi}/${sub}/${sub}_dwi_tmp.mif" \
-	"${dwi}/${sub}/${sub}_dwi_dn_tmp.mif" \
-	-noise "${dwi}/${sub}/${sub}_dwi_noise_tmp.nii.gz" \
+	"${dwi}/${sub}/${ses}/${sub}_dwi_tmp.mif" \
+	"${dwi}/${sub}/${ses}/${sub}_dwi_dn_tmp.mif" \
+	-noise "${dwi}/${sub}/${ses}/${sub}_dwi_noise_tmp.nii.gz" \
 	-force
 	
 	# Degibbs
 	mrdegibbs \
-	"${dwi}/${sub}/${sub}_dwi_dn_tmp.mif" \
-	"${dwi}/${sub}/${sub}_dwi_dg_tmp.nii.gz" \
+	"${dwi}/${sub}/${ses}/${sub}_dwi_dn_tmp.mif" \
+	"${dwi}/${sub}/${ses}/${sub}_dwi_dg_tmp.nii.gz" \
 	-force
 	
 	# Square degibbs
 	fslmaths \
-	"${dwi}/${sub}/${sub}_dwi_dg_tmp.nii.gz" \
+	"${dwi}/${sub}/${ses}/${sub}_dwi_dg_tmp.nii.gz" \
 	-sqr \
-	"${dwi}/${sub}/${sub}_dwi_dg_sqr_tmp.nii"
+	"${dwi}/${sub}/${ses}/${sub}_dwi_dg_sqr_tmp.nii"
 	
 	# Square noise
 	fslmaths \
-	"${dwi}/${sub}/${sub}_dwi_noise_tmp.nii.gz" \
+	"${dwi}/${sub}/${ses}/${sub}_dwi_noise_tmp.nii.gz" \
 	-sqr \
-	"${dwi}/${sub}/${sub}_dwi_noise_sqr_tmp.nii.gz"
+	"${dwi}/${sub}/${ses}/${sub}_dwi_noise_sqr_tmp.nii.gz"
 		
 	# Subtract sqaured noise from squared degibbs
 	fslmaths \
-	"${dwi}/${sub}/${sub}_dwi_dg_sqr_tmp.nii.gz" \
+	"${dwi}/${sub}/${ses}/${sub}_dwi_dg_sqr_tmp.nii.gz" \
 	-sub \
-	"${dwi}/${sub}/${sub}_dwi_noise_sqr_tmp.nii.gz" \
-	"${dwi}/${sub}/${sub}_dwi_noise_sub_dg_tmp.nii.gz"
+	"${dwi}/${sub}/${ses}/${sub}_dwi_noise_sqr_tmp.nii.gz" \
+	"${dwi}/${sub}/${ses}/${sub}_dwi_noise_sub_dg_tmp.nii.gz"
 	
 	# Square root the subtracted image (eddy input)
 	fslmaths \
-	"${dwi}/${sub}/${sub}_dwi_noise_sub_dg_tmp.nii.gz" \
+	"${dwi}/${sub}/${ses}/${sub}_dwi_noise_sub_dg_tmp.nii.gz" \
 	-sqrt \
-	"${dwi}/${sub}/${sub}_dwi.nii.gz"
+	"${dwi}/${sub}/${ses}/${sub}_dwi.nii.gz"
 	
 	# Clean up directory of temporary files
-	rm "${dwi}/${sub}/*tmp*"
+	rm "${dwi}/${sub}/${ses}/*tmp*"
 
 	# Create a b0 image
 	dwiextract \
-		"${dwi}/${sub}/${sub}_dwi.nii.gz" - -bzero -fslgrad \
-		"${dwi}/${sub}/${sub}_dwi.bvec" \
-		"${dwi}/${sub}/${sub}_dwi.bval" | \
-	mrmath - mean "${dwi}/${sub}/${sub}_dwi_b0.nii.gz" -axis 3
-
+		"${dwi}/${sub}/${ses}/${sub}_dwi.nii.gz" - -bzero -fslgrad \
+		"${dwi}/${sub}/${ses}/${sub}_dwi.bvec" \
+		"${dwi}/${sub}/${ses}/${sub}_dwi.bval" | \
+	mrmath - mean "${dwi}/${sub}/${ses}/${sub}_dwi_b0.nii.gz" -axis 3
+done
 done
