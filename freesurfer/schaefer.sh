0bbace91bfc3b6324754eaf00e791e1f5d5d14cc
andr-ws <108693884+andr-ws@users.noreply.github.com>
2024-12-05 17:46:20 +0000

    Update schaefer.sh

--- freesurfer/schaefer.sh
+++ freesurfer/schaefer.sh
@@ -24,32 +24,75 @@ for dir in ${dwi}/sub-*; do
         --tval ${SUBJECTS_DIR}/${sub}/label/${h}.Schaefer2018_${rois}Parcels_7Networks_order.annot
     done
           
-    # Convert annot to volume
-    mri_aparc2aseg \
-      --s ${sub} \
-      --o ${SUBJECTS_DIR}/${sub}/mri/Schaefer2018_${rois}Parcels_7Networks.mgz \
-      --annot Schaefer2018_${rois}Parcels_7Networks_order
+  # Extrapolate FS metrics (MIND networks)
+  --> Execute code here.
 
-    # Convert and reorient the parcellations and brain file
-    mri_convert \
-      --in_orientation LIA \
-      --out_orientation RAS \
-      -it mgz \
-      -ot nii \
-      ${SUBJECTS_DIR}/${sub}/mri/Schaefer2018_${rois}Parcels_7Networks.mgz \
-      ${SUBJECTS_DIR}/${sub}/mri/Schaefer2018_${rois}Parcels_7Networks.nii.gz
+  # ACT
+  mkdir ${dir}/fba/tractograms
 
-    # Apply native transform to the Schaefer parcellation
-    mkdir ${anat}/${sub}/parc
-      
-    antsApplyTransforms \
-      -d 3 \
-      -i ${SUBJECTS_DIR}/${sub}/mri/Schaefer2018_${rois}Parcels_7Networks.nii.gz \
-      -r ${anat}/${sub}/proc/${sub}_desc-res-1mm_bfc_T1w_brain.nii.gz \
-      -o ${anat}/${sub}/parc/${sub}_Schaefer2018_${rois}Parcels_7Networks_T1w.nii.gz \
-      -n NearestNeighbor \
-      -t ${SUBJECTS_DIR}/${sub}/mri/transforms/fs_2_nat_0GenericAffine.mat
-  done
+  # NOT SURE IF REQUIRED YET
+  # Map b0 to T1 (rigid)
+  #antsRegistrationSyN.sh \
+  #  -d 3 \
+  #  -f T1_bias-cor \
+  #  -m eddy_b0 \
+  #  -t r
+  # Invert
+  #antsApplyTransforms \
+  #  -i T1_bias-cor \
+  #  -r eddy_b0 \
+    
+
+  # 
+  5ttgen \
+    fsl \
+    ${anat}/${sub}/proc/${sub}_T1w-bias_cor_brain.nii.gz \
+    ${dir}/fba/tractograms/${sub}_5tt.mif \
+    -premasked \
+    -force
+
+  5tt2gmwmi \
+    ${dir}/fba/tractograms/${sub}_5tt.mif \
+    ${dir}/fba/tractograms/${sub}_gmwmi.mif \
+    -force
+
+  tckgen \
+    -angle 22.5 \
+    -maxlen 250 \
+    -minlen 10 \
+    -power 1.0 \
+    ${dir}/fba/proc/${sub}_wmfod_norm.mif \
+    -seed_gmwmi ${dir}/fba/proc/${sub}_gmwmi.mif \
+    -cutoff 0.10  \
+    -select 20000000 \
+    ${dir}/fba/tractograms/${sub}.tck \
+    -act ${dir}/tractograms/${sub}_5tt.mif \
+    -force
+
+  # SIFT tractogram
+  #tcksift \
+  #  ${dir}/fba/tractograms/${sub}.tck \
+  #  ${dir}/fba/proc/${sub}_wmfod_norm.mif \
+  #  ${dir}/fba/tractograms/${sub}_SIFT.tck \
+  #  -term_number 2000000 \
+  #  -force
+
+  tcksift2 \
+     ${dir}/fba/tractograms/${sub}_SIFT.tck \
+     ${dir}/fba/proc/${sub}_wmfod_norm.mif \
+     ${dir}/fba/tractograms/${sub}.txt \
+     -act ${dir}/tractograms/${sub}_5tt.mif
+
+  tck2connectome \
+    ${dir}/fba/tractograms//ACT/${sub}_SIFT.tck \
+    parc_image.mif \
+    ${fba}/subjects/${sub}/ACT/Schaefer_tck_400.csv \
+    -symmetric \
+    -tck_weights_in ${dir}/fba/tractograms/${sub}.txt
+
+  # Remove original tractogram
+  #rm ${dir}/fba/tractograms/${sub}.tck
+
 done
 
 
