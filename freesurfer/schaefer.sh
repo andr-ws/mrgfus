b617aa7ee44b1a7879f70727ef22ea29f9cb837a
andr-ws <108693884+andr-ws@users.noreply.github.com>
2024-12-06 10:14:24 +0000

    Update schaefer.sh

--- freesurfer/schaefer.sh
+++ freesurfer/schaefer.sh
@@ -12,62 +12,63 @@ export SUBJECTS_DIR=${der}/freesurfer
 
 for dir in ${dwi}/sub-*; do
   sub=$(basename ${dir})
-        
-  # Resample the Schaefer parcellation to subject surface - generate annot files for multiple resolutions
-  for rois in 400 600 800; do
-    for h in lh rh; do
-      mri_surf2surf \
-        --hemi ${h} \
-        --srcsubject fsaverage \
-        --trgsubject ${sub} \
-        --sval-annot ${SUBJECTS_DIR}/fsaverage/label/${h}.Schaefer2018_${rois}Parcels_7Networks_order.annot \
-        --tval ${SUBJECTS_DIR}/${sub}/label/${h}.Schaefer2018_${rois}Parcels_7Networks_order.annot
-    done
+
+  for ses in ses-preop ses1-postop ses2-postop; do
+    # Resample the Schaefer parcellation to subject surface - generate annot files for multiple resolutions
+    for rois in 400 600 800; do
+      for h in lh rh; do
+        mri_surf2surf \
+          --hemi ${h} \
+          --srcsubject fsaverage \
+          --trgsubject ${sub} \
+          --sval-annot ${SUBJECTS_DIR}/fsaverage/label/${h}.Schaefer2018_${rois}Parcels_7Networks_order.annot \
+          --tval ${SUBJECTS_DIR}/${sub}/label/${h}.Schaefer2018_${rois}Parcels_7Networks_order.annot
+      done
+      
           
-  # Extrapolate FS metrics (MIND networks)
-  --> Execute code here.
+    # Extrapolate FS metrics (MIND networks)
+    --> Execute code here.
 
-  # ACT
-  mkdir ${dir}/fba/tractograms
+    # ACT
+    mkdir ${dir}/fba/tractograms
 
-  # NOT SURE IF REQUIRED YET
-  # Map b0 to T1 (rigid)
-  #antsRegistrationSyN.sh \
-  #  -d 3 \
-  #  -f T1_bias-cor \
-  #  -m eddy_b0 \
-  #  -t r
-  # Invert
-  #antsApplyTransforms \
-  #  -i T1_bias-cor \
-  #  -r eddy_b0 \
+    # NOT SURE IF REQUIRED YET
+    # Map b0 to T1 (rigid)
+    #antsRegistrationSyN.sh \
+      #  -d 3 \
+      #  -f T1_bias-cor \
+      #  -m eddy_b0 \
+      #  -t r
+    # Invert
+    #antsApplyTransforms \
+      #  -i T1_bias-cor \
+      #  -r eddy_b0 \
     
+    # Use brain extracted FS output
+    5ttgen \
+      fsl \
+      ${SUBJECTS_DIR}/${ses}/${sub}_brain.nii.gz \
+      ${dir}/fba/tractograms/${sub}_5tt.mif \
+      -premasked \
+      -force
 
-  # 
-  5ttgen \
-    fsl \
-    ${anat}/${sub}/proc/${sub}_T1w-bias_cor_brain.nii.gz \
-    ${dir}/fba/tractograms/${sub}_5tt.mif \
-    -premasked \
-    -force
+    5tt2gmwmi \
+      ${dir}/fba/tractograms/${sub}_5tt.mif \
+      ${dir}/fba/tractograms/${sub}_gmwmi.mif \
+      -force
 
-  5tt2gmwmi \
-    ${dir}/fba/tractograms/${sub}_5tt.mif \
-    ${dir}/fba/tractograms/${sub}_gmwmi.mif \
-    -force
-
-  tckgen \
-    -angle 22.5 \
-    -maxlen 250 \
-    -minlen 10 \
-    -power 1.0 \
-    ${dir}/fba/proc/${sub}_wmfod_norm.mif \
-    -seed_gmwmi ${dir}/fba/proc/${sub}_gmwmi.mif \
-    -cutoff 0.10  \
-    -select 20000000 \
-    ${dir}/fba/tractograms/${sub}.tck \
-    -act ${dir}/tractograms/${sub}_5tt.mif \
-    -force
+    tckgen \
+      -angle 22.5 \
+      -maxlen 250 \
+      -minlen 10 \
+      -power 1.0 \
+      ${dir}/fba/proc/${sub}_wmfod_norm.mif \
+      -seed_gmwmi ${dir}/fba/proc/${sub}_gmwmi.mif \
+      -cutoff 0.10  \
+      -select 20000000 \
+      ${dir}/fba/tractograms/${sub}.tck \
+      -act ${dir}/tractograms/${sub}_5tt.mif \
+      -force
 
   # SIFT tractogram
   #tcksift \
@@ -77,11 +78,11 @@ for dir in ${dwi}/sub-*; do
   #  -term_number 2000000 \
   #  -force
 
-  tcksift2 \
-     ${dir}/fba/tractograms/${sub}_SIFT.tck \
-     ${dir}/fba/proc/${sub}_wmfod_norm.mif \
-     ${dir}/fba/tractograms/${sub}.txt \
-     -act ${dir}/tractograms/${sub}_5tt.mif
+    tcksift2 \
+      ${dir}/fba/tractograms/${sub}_SIFT.tck \
+      ${dir}/fba/proc/${sub}_wmfod_norm.mif \
+      ${dir}/fba/tractograms/${sub}.txt \
+      -act ${dir}/tractograms/${sub}_5tt.mif
 
   tck2connectome \
     ${dir}/fba/tractograms//ACT/${sub}_SIFT.tck \
