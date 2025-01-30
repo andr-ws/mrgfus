c5bad74442b4914dd0d2eb9e3fce51a5b9210791
andr-ws <108693884+andr-ws@users.noreply.github.com>
2025-01-30 14:26:02 +0000

    Update fod.sh

--- dwi/fod.sh
+++ dwi/fod.sh
@@ -1,102 +1,102 @@
 #! /bin/bash
-
+set +H  # Disable history expansion
 export FREESURFER_HOME=/Applications/freesurfer
 source $FREESURFER_HOME/SetUpFreeSurfer.sh
 
 # Compute FOD response functions for DWI
-
 base=~/imaging/datasets/mrgfus
 raw=${base}/rawdata
-dwi=${base}/derivatives/dwi
+dwi=${base}/derivatives/data/dwi
+fba=${base}/derivatives/projects/fba
 
 for dir in ${dwi}/sub-*; do
   sub=$(basename ${dir})
 
-  if [ ! -d "${eddy}" ]; then
-    echo "Skipping subject ${sub}, dwi/eddy directory not found."
-    continue
-  fi
+  for ses in ses-01 ses-02 ses-03; do
 
-  for ses in ses-preop ses1-postop ses2-postop; do
+    if [ ! -d ${dir}/${ses}/eddy ]; then
+      echo "Skipping subject ${sub} ${ses} - this directory is not found!"
+      continue
+    fi
   
     echo "Processing subject ${sub} for session ${ses}"
-    bval=${raw}/${sub}/${ses}/dwi/${sub}_dwi.bval
-    mkdir -p ${dir}/${ses}/fod
+    
+    mkdir -p ${fba}/data/${sub}/${ses}/fod
   
-    # Convert to mif format
+    # Convert eddy data
     mrconvert \
       ${dir}/${ses}/eddy/${sub}_dwi_edc.nii.gz \
-      ${dir}/${ses}/fod/${sub}_dwi.mif \
+      ${fba}/data/${sub}/${ses}/fod/tmp_${sub}_dwi.mif \
       -fslgrad \
-      ${eddy}/${sub}_dwi_edc.eddy_rotated_bvecs \
-      ${bval} \
-      -force
+      ${dir}/${ses}/eddy/${sub}_dwi_edc.eddy_rotated_bvecs \
+      ${dir}/${ses}/${sub}_${ses}_acq-dwi.bval \
     
-    # Bias field correct
+    # Bias correct
     dwibiascorrect \
       ants \
-      ${dir}/${ses}/fod/${sub}_dwi.mif \
-      ${dir}/${ses}/fod/tmp_${sub}_dwi_dn.mif -force
+      ${fba}/data/${sub}/${ses}/fod/tmp_${sub}_dwi.mif \
+      ${fba}/data/${sub}/${ses}/fod/tmp_${sub}_dwi_dn.mif
 
     # Compute average tissue-response function
     dwi2response \
       dhollander \
-      ${dir}/${ses}/fod/tmp_${sub}_dwi_dn.mif \
-      ${dir}/${ses}/fod/response_wm.txt \
-      ${dir}/${ses}/fod/response_gm.txt \
-      ${dir}/${ses}/fod/response_csf.txt -force
+      ${fba}/data/${sub}/${ses}/fod/tmp_${sub}_dwi_dn.mif \
+      ${fba}/data/${sub}/${ses}/fod/response_wm.txt \
+      ${fba}/data/${sub}/${ses}/fod/response_gm.txt \
+      ${fba}/data/${sub}/${ses}/fod/response_csf.txt
 
     # Regrid to voxel size 1.25
     mrgrid \
-      ${dir}/${ses}/fod/tmp_${sub}_dwi_dn.mif \
+      ${fba}/data/${sub}/${ses}/fod/tmp_${sub}_dwi_dn.mif \
       regrid -vox 1.25 \
-      ${dir}/${ses}/fod/tmp_${sub}_dwi_us.mif -force
+      ${fba}/data/${sub}/${ses}/fod/${sub}_dwi_us.mif
 
-    # Extract upsampled b0 for ACT
+    # Extract upsampled b0
     dwiextract \
-      ${dir}/${ses}/fod/tmp_${sub}_dwi_us.mif \
-      - -bzero | mrmath - mean ${dir}/${ses}/fod/${sub}_b0_us.mif \
+      ${fba}/data/${sub}/${ses}/fod/${sub}_dwi_us.mif \
+      - -bzero | mrmath - mean ${fba}/data/${sub}/${ses}/fod/tmp_${sub}_b0_us.mif \
       -axis 3
 
-    mrconvert ${dir}/${ses}/fod/${sub}_b0_us.mif ${dir}/${ses}/fod/${sub}_b0_us.nii.gz
+    mrconvert \
+      ${fba}/data/${sub}/${ses}/fod/tmp_${sub}_b0_us.mif \
+      ${fba}/data/${sub}/${ses}/fod/tmp_${sub}_b0_us.nii.gz
 
     # Brain extract and create mask
     mri_synthstrip \
-      -i ${dir}/${ses}/fod/${sub}_b0_us.nii.gz \
-      -o ${dir}/${ses}/fod/${sub}_b0_brain_us.nii.gz \
-      -m ${dir}/${ses}/fod/${sub}_b0_brain_mask_us.nii.gz
+      -i ${fba}/data/${sub}/${ses}/fod/tmp_${sub}_b0_us.nii.gz \
+      -o ${fba}/data/${sub}/${ses}/fod/tmp_${sub}_b0_brain_us.nii.gz \
+      -m ${fba}/data/${sub}/${ses}/fod/tmp_${sub}_b0_brain_mask_us.nii.gz
 
     # Convert brain mask to mif format
     mrconvert \
-      ${dir}/${ses}/fod/${sub}_b0_brain_mask_us.nii.gz \
-      ${dir}/${ses}/fod/${sub}_b0_brain_mask_us.mif
+      ${fba}/data/${sub}/${ses}/fod/tmp_${sub}_b0_brain_mask_us.nii.gz \
+      ${fba}/data/${sub}/${ses}/fod/${sub}_b0_brain_mask_us.mif
 
-    rm ${dir}/${ses}/fod/*tmp*
+    rm ${fba}/data/${sub}/${ses}/fod/tmp*
       
   done
 done
 
-fba=${base}/derivatives/fba
-mkdir -p ${fba}/response
-
-# Create tissue RF's
+# Create group tissue response functions
+mkdir -p ${fba}/rfuncs
 
 for tissue in wm gm csf; do
-  responsemean ${dwi}/*/*/fod/response_${tissue}.txt ${fba}/response/group_response_${tissue}.txt
+  responsemean ${fba}/*/*/*/fod/response_${tissue}.txt ${fba}/rfuncs/group_response_${tissue}.txt
 done
 
-for dir in ${dwi}/sub-*; do
+# Create individual tissue response functions
+for dir in ${fba}/data/sub-*; do
   sub=$(basename ${dir})
 
-  for ses in ses-preop ses1-postop ses2-postop; do
+  for ses in ses-01 ses-02 ses-03; do
     ss3t_csd_beta1 \
-      ${dir}/${ses}/fod/tmp_${sub}_dwi_us.mif \
-      ${fba}/response/group_response_wm.txt \
+      ${dir}/${ses}/fod/${sub}_dwi_us.mif \
+      ${fba}/rfuncs/group_response_wm.txt \
       ${dir}/${ses}/fod/tmp_${sub}_wmfod.mif \
-      ${fba}/response/group_response_gm.txt \
+      ${fba}/rfuncs/group_response_gm.txt \
       ${dir}/${ses}/fod/tmp_${sub}_gmfod.mif \
-      ${fba}/response/group_response_csf.txt \
-      ${dir}/${ses}/fod/tmp_${sub}_csffod.txt \
+      ${fba}/rfuncs/group_response_csf.txt \
+      ${dir}/${ses}/fod/tmp_${sub}_csffod.mif \
       -mask ${dir}/${ses}/fod/${sub}_b0_brain_mask_us.mif
 
     mtnormalise \
@@ -104,7 +104,8 @@ for dir in ${dwi}/sub-*; do
       ${dir}/${ses}/fod/tmp_${sub}_gmfod.mif ${dir}/${ses}/fod/${sub}_gmfod.mif
       ${dir}/${ses}/fod/tmp_${sub}_csffod.mif ${dir}/${ses}/fod/${sub}_csffod.mif
 
-    rm ${dir}/${ses}/fod/*tmp*
+    rm ${dir}/${ses}/fod/${sub}_dwi_us.mif
+    rm ${dir}/${ses}/fod/tmp_${sub}_wmfod.mif ${dir}/${ses}/fod/tmp_${sub}_gmfod.mif ${dir}/${ses}/fod/tmp_${sub}_csffod.mif 
   done
 done
 
