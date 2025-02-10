ce19ea84a61982016068ef53eeeb3c13bcac571a
andr-ws <108693884+andr-ws@users.noreply.github.com>
2025-02-10 12:08:09 +0000

    Create pre-op_vols.sh

---
+++ analysis/pre-op_vols.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+# Code to extract "measures of atrophy" as a pre-op "predictor" of surgical outcome
+
+freesurfer/starts/brainvol.stats # contains brainseg no ventricles and ventricle volumes
+
+
+# Division perhaps
