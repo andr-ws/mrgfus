4cd71e6f66015e09095f37032fd08e3899e6395e
andr-ws <108693884+andr-ws@users.noreply.github.com>
2025-02-12 14:29:50 +0000

    Update genParc.sh

--- analysis/genParc.sh
+++ analysis/genParc.sh
@@ -8,9 +8,9 @@ export SUBJECTS_DIR=${der}/data/freesurfer
 
 for dir in ${der}/data/freesurfer/sub-*; do
   sub=$(basename ${dir})
-  for ses in ses-01 ses-02 ses-03
+  for ses in ses-01 ses-02 ses-03; do
+    
     parc=${dir}/${ses}/parcellations
-  
     mkdir ${parc}
 
     # Resample the Schaefer parcellations to subject surface - generate annot files for multiple resolutions (done in SchaeferResample.sh)      
@@ -33,9 +33,7 @@ for dir in ${der}/data/freesurfer/sub-*; do
     # Compute (rigid) us_b0 to synthetic space xfm
     mri_convert ${dir}/${ses}/mri/brain.norm.mgz ${parc}/tmp_${sub}_fs_brain.nii.gz
     fslreorient2std ${parc}/tmp_${sub}_fs_brain.nii.gz ${parc}/${sub}_fs_brain.nii.gz
-
-    rm ${parc}/tmp*
-
+    
     antsRegistrationSyN.sh -d 3 -t r -n 16 \
     -f ${parc}/${sub}_fs_brain.nii.gz \
     -m ${parc}/${sub}_b0_us_brain.nii.gz \
@@ -44,31 +42,34 @@ for dir in ${der}/data/freesurfer/sub-*; do
     # Transform parcellations to dwi space
     antsApplyTransforms -d 3 \
     -r ${parc}/${sub}_b0_us_brain.nii.gz \
-    -i ${parc}/${sub}_Schaefer2018_${rois}Parcels_7Networks.nii.gz \
-    -o ${dwi}/parcellations/${sub}_Schaefer2018_${rois}Parcels_7Networks_space-dwi.nii.gz \
-    -t [${dwi}/parcellations/b0_to_sb0_0GenericAffine.mat,1] \
+    -i ${parc}/${sub}_Schaefer2018_400Parcels_7Networks.nii.gz \
+    -o ${parc}/${sub}_Schaefer2018_400Parcels_7Networks_space-dwi.nii.gz \
+    -t [${parc}/${sub}_b0_to_fs_0GenericAffine.mat,1] \
     -n GenericLabel[linear]
 
     # Remove non-cortical labels
     fslmaths \
-    ${dwi}/parcellations/${sub}_Schaefer2018_${rois}Parcels_7Networks_space-dwi.nii.gz \
+    ${parc}/${sub}_Schaefer2018_400Parcels_7Networks_space-dwi.nii.gz \
     -thr 1000 \
-    ${dwi}/parcellations/${sub}_Schaefer2018_${rois}Parcels_7Networks_space-dwi.nii.gz
+    ${parc}/${sub}_Schaefer2018_400Parcels_7Networks_space-dwi.nii.gz
 
     # Remove medial walls
-    fslmaths ${dwi}/parcellations/${sub}_Schaefer2018_${rois}Parcels_7Networks_space-dwi.nii.gz \
-    -thr 1000 -uthr 1000 ${dwi}/parcellations/tmp_Schaefer2018_${rois}Parcels_7Networks_space-dwi_exclude1.nii.gz
-    fslmaths ${dwi}/parcellations/${sub}_Schaefer2018_${rois}Parcels_7Networks_space-dwi.nii.gz \
-    -thr 2000 -uthr 2000 ${dwi}/parcellations/tmp_Schaefer2018_${rois}Parcels_7Networks_space-dwi_exclude2.nii.gz
-    fslmaths ${dwi}/parcellations/tmp_Schaefer2018_${rois}Parcels_7Networks_space-dwi_exclude1.nii.gz \
-    -add ${dwi}/parcellations/tmp_Schaefer2018_${rois}Parcels_7Networks_space-dwi_exclude2.nii.gz \
-    ${dwi}/parcellations/tmp_Schaefer2018_${rois}Parcels_7Networks_space-dwi_exclude.nii.gz
-    fslmaths ${dwi}/parcellations/${sub}_Schaefer2018_${rois}Parcels_7Networks_space-dwi.nii.gz \
-    -sub ${dwi}/parcellations/tmp_Schaefer2018_${rois}Parcels_7Networks_space-dwi_exclude.nii.gz \
-    ${dwi}/parcellations/${sub}_Schaefer2018_${rois}Parcels_7Networks_space-dwi.nii.gz
+    fslmaths ${parc}/${sub}_Schaefer2018_400Parcels_7Networks_space-dwi.nii.gz \
+    -thr 1000 -uthr 1000 ${parc}/tmp_${sub}_Schaefer2018_400Parcels_7Networks_space-dwi_ex1.nii.gz
+    
+    fslmaths ${parc}/${sub}_Schaefer2018_400Parcels_7Networks_space-dwi.nii.gz \
+    -thr 2000 -uthr 2000 ${parc}/tmp_${sub}_Schaefer2018_400Parcels_7Networks_space-dwi_ex2.nii.gz
+    
+    fslmaths ${parc}/tmp_${sub}_Schaefer2018_400Parcels_7Networks_space-dwi_ex1.nii.gz \
+    -add ${parc}/tmp_${sub}_Schaefer2018_400Parcels_7Networks_space-dwi_ex2.nii.gz \
+    ${parc}/tmp_${sub}_Schaefer2018_400Parcels_7Networks_space-dwi_ex3.nii.gz
+    
+    fslmaths ${parc}/${sub}_Schaefer2018_400Parcels_7Networks_space-dwi.nii.gz \
+    -sub ${parc}/tmp_${sub}_Schaefer2018_400Parcels_7Networks_space-dwi_ex3.nii.gz
+    ${parc}/${sub}_Schaefer2018_400Parcels_7Networks_space-dwi.nii.gz
 
     # Clean up
-    rm ${dwi}/parcellations/tmp*.nii.gz
+    rm ${parc}/tmp*.nii.gz
     
   done
 done
