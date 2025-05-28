ec27474fa91db97de3b46751c7825ad4c362f6a1
andr-ws <108693884+andr-ws@users.noreply.github.com>
2025-05-28 20:51:03 +0100

    Update process_msn.sh

--- freesurfer/process_msn.sh
+++ freesurfer/process_msn.sh
@@ -1,21 +1,28 @@
+#! /bin/bash
 
+# Code to resample fsaverage 500 ROI DKT atlas onto longitudinal pipeline surfaces
 
 for dir in ${SUBJECTS_DIR}/sub-*; do
   sub=$(basename ${dir})
 
   for hemi in lh rh; do
-    pathname=${sub}_${ses}.long.sub-001_base
     parc=500.aparc
+    
     for ses in ses-01 ses-02 ses-03; do
-
-      # need to enter code here for transforming fsaverage surface
-      # generating annot files etc
-
+      pathname=${sub}_${ses}.long.${sub}_base
+      
+      mri_surf2surf \
+        --hemi ${hemi} \
+        --srcsubject fsaverage \
+        --trgsubject ${sub}/long/${pathname} \
+        --sval-annot ${SUBJECTS_DIR}/fsaverage/label/${hemi}.${parc}.annot \
+        --tval ${sub}/long/${pathname}/label/${hemi}.${parc}.annot
+    
       mris_anatomical_stats \
-      -a ${SUBJECTS_DIR}/${sub}/long/${pathname}/label/${hemi}.${parc}.annot \
-      -f ${SUBJECTS_DIR}/${sub}/long/${pathname}/stats/${hemi}.${parc}.stats  \
-      ${sub}/long/${pathname} \
-      ${hemi}
+        -a ${SUBJECTS_DIR}/${sub}/long/${pathname}/label/${hemi}.${parc}.annot \
+        -f ${SUBJECTS_DIR}/${sub}/long/${pathname}/stats/${hemi}.${parc}.stats  \
+        ${sub}/long/${pathname} \
+        ${hemi}
     done
   done
 done
