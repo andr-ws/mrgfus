434a1b91bf2c12ee5aa2995e95b40328a8c39fbe
andr-ws <108693884+andr-ws@users.noreply.github.com>
2025-06-03 10:33:27 +0100

    Create lesions_flip.m

---
+++ preproc/lesions_flip.m
@@ -0,0 +1,12 @@
+% MATLAB code to flip rh lesions to lh
+
+ids = readlines('/Volumes/LA_4TB/datasets/mrgfus/derivatives/study_files/rh_subs.txt');
+
+for i = 1:length(ids)
+    id = ids{i};
+    
+    input_file = sprintf('/Users/neuro-239/imaging/datasets/mrgfus/derivatives/lesions/masks/mni/sub-%s/sub-%s_lesion.nii.gz', id, id);
+    output_file = sprintf('/Users/neuro-239/imaging/datasets/mrgfus/derivatives/lesions/masks/mni/sub-%s/sub-%s_flesion.nii.gz', id, id);
+    
+    ea_flip_lr_nonlinear(input_file, output_file, 1);
+end
