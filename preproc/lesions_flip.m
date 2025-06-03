14043f51fe389da46fcd76de8153547f0648dd8b
andr-ws <108693884+andr-ws@users.noreply.github.com>
2025-06-03 12:14:35 +0100

    Update lesions_flip.m

--- preproc/lesions_flip.m
+++ preproc/lesions_flip.m
@@ -5,8 +5,8 @@ ids = readlines('/Volumes/LA_4TB/datasets/mrgfus/derivatives/study_files/rh_subs
 for i = 1:length(ids)
     id = ids{i};
     
-    input_file = sprintf('/Users/neuro-239/imaging/datasets/mrgfus/derivatives/lesions/masks/mni/sub-%s/sub-%s_lesion.nii.gz', id, id);
-    output_file = sprintf('/Users/neuro-239/imaging/datasets/mrgfus/derivatives/lesions/masks/mni/sub-%s/sub-%s_flesion.nii.gz', id, id);
+    input_file = sprintf('/Users/neuro-239/imaging/datasets/mrgfus/derivatives/lesions/masks/mni/%s/%s_lesion.nii.gz', id, id);
+    output_file = sprintf('/Users/neuro-239/imaging/datasets/mrgfus/derivatives/lesions/masks/mni/%s/%s_flesion.nii.gz', id, id);
     
     ea_flip_lr_nonlinear(input_file, output_file, 1);
 end
