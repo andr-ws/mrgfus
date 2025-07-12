9df645f33fb314fa33dbef635cc07a0e1a0db696
andr-ws <108693884+andr-ws@users.noreply.github.com>
2025-07-12 18:17:29 +0100

    Update fba_sweetspot.sh

--- dwi/fba_sweetspot.sh
+++ dwi/fba_sweetspot.sh
@@ -1,2 +1,7 @@
 # Code here to perform FBA analysis based on lesion mapping results
 
+mri_synthmorph \
+  apply \
+  -m nearest /Users/a9ws/imaging/datasets/mrgfus/derivatives/fba/template/study_template/wmfod/MNI-wmfod_template_warp.nii.gz \
+  /Users/a9ws/imaging/datasets/mrgfus/derivatives/fba/template/study_template/tracts/nmap_thr.nii.gz \
+  /Users/a9ws/imaging/datasets/mrgfus/derivatives/fba/template/study_template/tracts/nmap_thr-wmfod.nii.gz
