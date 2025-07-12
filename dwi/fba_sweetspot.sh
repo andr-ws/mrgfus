bd9cc3894e03d5f2de2ff037a5013cbaeebe60d6
andr-ws <108693884+andr-ws@users.noreply.github.com>
2025-07-12 18:22:41 +0100

    Update fba_sweetspot.sh

--- dwi/fba_sweetspot.sh
+++ dwi/fba_sweetspot.sh
@@ -5,3 +5,11 @@ mri_synthmorph \
   -m nearest /Users/a9ws/imaging/datasets/mrgfus/derivatives/fba/template/study_template/wmfod/MNI-wmfod_template_warp.nii.gz \
   /Users/a9ws/imaging/datasets/mrgfus/derivatives/fba/template/study_template/tracts/nmap_thr.nii.gz \
   /Users/a9ws/imaging/datasets/mrgfus/derivatives/fba/template/study_template/tracts/nmap_thr-wmfod.nii.gz
+
+tckgen \
+  /Users/a9ws/imaging/datasets/mrgfus/derivatives/fba/template/study_template/wmfod/wmfod_template.mif \
+  /Users/a9ws/imaging/datasets/mrgfus/derivatives/fba/template/study_template/tracts/dn-nmap.tck \
+  -seed_image /Users/a9ws/imaging/datasets/mrgfus/derivatives/fba/template/study_template/tracts/R_DN.mif \
+  -select 10000 \
+  -include /Users/a9ws/imaging/datasets/mrgfus/derivatives/fba/template/study_template/tracts/nmap_thr-wmfod.nii.gz \
+  -seed_unidirectional -stop
